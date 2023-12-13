a75f228bc27d95c33f2679e509f8e1f6c2cf1270
dernn <fajj@mail.ru>
2023-12-13 23:09:17 +0600

    Fix task__django_shell encoding

--- task__django_shell.py
+++ task__django_shell.py
@@ -2,33 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from news.models import *
 
-# 1. Создать двух пользователей (с помощью метода User.objects.create_user('username')).
+# 1. РЎРѕР·РґР°С‚СЊ РґРІСѓС… РїРѕР»СЊР·РѕРІР°С‚РµР»РµР№ (СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґР° User.objects.create_user('username')).
 base1 = User.objects.create_user('username8')
 base2 = User.objects.create_user('username9')
 base3 = User.objects.create_user('username0')
 
-# 2. Создать два объекта модели Author, связанные с пользователями.
+# 2. РЎРѕР·РґР°С‚СЊ РґРІР° РѕР±СЉРµРєС‚Р° РјРѕРґРµР»Рё Author, СЃРІСЏР·Р°РЅРЅС‹Рµ СЃ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏРјРё.
 user1 = Author.objects.create(user=base1)
 user2 = Author.objects.create(user=base2)
 
-# 3. Добавить 4 категории в модель Category.
+# 3. Р”РѕР±Р°РІРёС‚СЊ 4 РєР°С‚РµРіРѕСЂРёРё РІ РјРѕРґРµР»СЊ Category.
 cate1 = Category.objects.create(name='Education')
 cate2 = Category.objects.create(name='Sony')
 cate3 = Category.objects.create(name='Films')
 cate4 = Category.objects.create(name='Politics')
 
-# 4. Добавить 2 статьи и 1 новость.
+# 4. Р”РѕР±Р°РІРёС‚СЊ 2 СЃС‚Р°С‚СЊРё Рё 1 РЅРѕРІРѕСЃС‚СЊ.
 post1 = Post.objects.create(author=user1, size=Post.article, headline='Something in the World', content='Some text.')
 post2 = Post.objects.create(author=user2, size=Post.article, headline='Something Again', content='Another text.')
 post3 = Post.objects.create(author=user2, size=Post.news, headline='Again & again', content='Another text.')
 
-# 5. Присвоить им категории (как минимум в одной статье/новости должно быть не меньше 2 категорий).
+# 5. РџСЂРёСЃРІРѕРёС‚СЊ РёРј РєР°С‚РµРіРѕСЂРёРё (РєР°Рє РјРёРЅРёРјСѓРј РІ РѕРґРЅРѕР№ СЃС‚Р°С‚СЊРµ/РЅРѕРІРѕСЃС‚Рё РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ РЅРµ РјРµРЅСЊС€Рµ 2 РєР°С‚РµРіРѕСЂРёР№).
 post1.category.add(Category.objects.get(pk=cate4.pk))
 post2.category.add(Category.objects.get(pk=cate4.pk))
 post2.category.add(Category.objects.get(pk=cate2.pk))
 post3.category.add(Category.objects.get(pk=cate3.pk))
 
-# 6. Создать как минимум 4 комментария к разным объектам модели Post (в каждом объекте должен быть как минимум один комментарий).
+# 6. РЎРѕР·РґР°С‚СЊ РєР°Рє РјРёРЅРёРјСѓРј 4 РєРѕРјРјРµРЅС‚Р°СЂРёСЏ Рє СЂР°Р·РЅС‹Рј РѕР±СЉРµРєС‚Р°Рј РјРѕРґРµР»Рё Post
+# (РІ РєР°Р¶РґРѕРј РѕР±СЉРµРєС‚Рµ РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ РєР°Рє РјРёРЅРёРјСѓРј РѕРґРёРЅ РєРѕРјРјРµРЅС‚Р°СЂРёР№).
 comm1 = Comment.objects.create(post=post1, user=base1, content='Some comment')
 comm2 = Comment.objects.create(post=post1, user=base2, content='Some comment')
 comm3 = Comment.objects.create(post=post2, user=base2, content='Comment')
@@ -36,7 +37,7 @@ comm4 = Comment.objects.create(post=post3, user=base1, content='Comment again')
 comm5 = Comment.objects.create(post=post3, user=base3, content='Comment forever')
 comm6 = Comment.objects.create(post=post2, user=base3, content='Comment')
 
-# 7. Применяя функции like() и dislike() к статьям/новостям и комментариям, скорректировать рейтинги этих объектов.
+# 7. РџСЂРёРјРµРЅСЏСЏ С„СѓРЅРєС†РёРё like() Рё dislike() Рє СЃС‚Р°С‚СЊСЏРј/РЅРѕРІРѕСЃС‚СЏРј Рё РєРѕРјРјРµРЅС‚Р°СЂРёСЏРј, СЃРєРѕСЂСЂРµРєС‚РёСЂРѕРІР°С‚СЊ СЂРµР№С‚РёРЅРіРё СЌС‚РёС… РѕР±СЉРµРєС‚РѕРІ.
 post1.like()
 post1.like()
 post1.like()
@@ -121,21 +122,19 @@ comm6.dislike()
 comm6.like()
 comm6.like()
 
-# 8. Обновить рейтинги пользователей.
+# 8. РћР±РЅРѕРІРёС‚СЊ СЂРµР№С‚РёРЅРіРё РїРѕР»СЊР·РѕРІР°С‚РµР»РµР№.
 user1.update_rating()
 user2.update_rating()
 
-# 9. Вывести username и рейтинг лучшего пользователя (применяя сортировку и возвращая поля первого объекта).
+# 9. Р’С‹РІРµСЃС‚Рё username Рё СЂРµР№С‚РёРЅРі Р»СѓС‡С€РµРіРѕ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ (РїСЂРёРјРµРЅСЏСЏ СЃРѕСЂС‚РёСЂРѕРІРєСѓ Рё РІРѕР·РІСЂР°С‰Р°СЏ РїРѕР»СЏ РїРµСЂРІРѕРіРѕ РѕР±СЉРµРєС‚Р°).
 Author.objects.order_by('-rating').values('user__username', 'rating').first()
 
-# 10. Вывести дату добавления, username автора, рейтинг, заголовок и превью лучшей статьи, основываясь на лайках/дислайках к этой статье.
+# 10. Р’С‹РІРµСЃС‚Рё РґР°С‚Сѓ РґРѕР±Р°РІР»РµРЅРёСЏ, username Р°РІС‚РѕСЂР°, СЂРµР№С‚РёРЅРі, Р·Р°РіРѕР»РѕРІРѕРє Рё РїСЂРµРІСЊСЋ Р»СѓС‡С€РµР№ СЃС‚Р°С‚СЊРё,
+# РѕСЃРЅРѕРІС‹РІР°СЏСЃСЊ РЅР° Р»Р°Р№РєР°С…/РґРёСЃР»Р°Р№РєР°С… Рє СЌС‚РѕР№ СЃС‚Р°С‚СЊРµ.
 post = Post.objects.order_by('-rating').first()
 post_dict = Post.objects.order_by('-rating').values('pub_date', 'author__user__username', 'rating', 'headline').first()
 post_dict.update({'preview': post.preview()})
 post_dict
 
-# 11. Вывести все комментарии (дата, пользователь, рейтинг, текст) к этой статье.
+# 11. Р’С‹РІРµСЃС‚Рё РІСЃРµ РєРѕРјРјРµРЅС‚Р°СЂРёРё (РґР°С‚Р°, РїРѕР»СЊР·РѕРІР°С‚РµР»СЊ, СЂРµР№С‚РёРЅРі, С‚РµРєСЃС‚) Рє СЌС‚РѕР№ СЃС‚Р°С‚СЊРµ.
 post.comment_set.all().values('pub_date', 'user__username', 'rating', 'content')
-
-
-
