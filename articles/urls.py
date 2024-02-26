613b2e1c8fd10e0828988a92e01ee0e1a0f5f5bb
dernn <fajj@mail.ru>
2024-02-26 12:53:29 +0600

    articles update/delete link fix for upd. requirements [D7.7]

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,6 @@ from news.views import PostCreateView, PostUpdateView, PostDeleteView
 urlpatterns = [
     # for upd. requirements D7.7
     path('add/', PostCreateView.as_view(), name='articles_create'),
-    # path('<int:pk>/edit/', PostUpdateView.as_view(), name='articles_edit'),
-    # path('<int:pk>/delete/', PostDeleteView.as_view(), name='articles_delete'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='articles_update'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='articles_delete'),
 ]
