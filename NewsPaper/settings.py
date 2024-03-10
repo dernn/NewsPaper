c74edf4481284c6d6c60235556c2e914c9295101
dernn <fajj@mail.ru>
2024-03-10 20:39:59 +0600

    switched to local Redis server (fix mailing)

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -205,8 +205,8 @@ APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
 
 # for D10.3: Celery settings
-CELERY_BROKER_URL = f'redis://:{config["REDIS_PASSWORD"]}@{config["REDIS_ENDPOINT"]}'
-CELERY_RESULT_BACKEND = f'redis://:{config["REDIS_PASSWORD"]}@{config["REDIS_ENDPOINT"]}'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
