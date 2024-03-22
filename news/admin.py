2b8864ab2aee49023b091e06b9bca415abad6d9b
dernn <fajj@mail.ru>
2024-03-22 13:45:51 +0600

    ModelAdmin.list_display, list_filter [D14.5]

--- news/admin.py
+++ news/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
+# удобный вывод и фильтры для админки [D14.5]
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pub_date', 'headline', 'author')  # список со всеми полями, которые будут в таблице
+    list_filter = ('author',)  # простой фильтр для админки
+    search_fields = ('pub_date', 'category__name')  # поиск по дате и категории
+
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
